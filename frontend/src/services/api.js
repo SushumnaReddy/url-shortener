53bae10a598564ee221d642e2783e95fe17493f2
SushumnaReddy <sushumnareddy627@example.com>
2026-06-27 23:46:26 +0530

    Update frontend backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,16 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://url-shortener-wly.onrender.com";
 
-export async function shortenUrl(
-  originalUrl,
-  customAlias,
-  expiryDays
-) {
+export async function shortenUrl(originalUrl, customAlias, expiryDays) {
   const response = await fetch(`${API_BASE_URL}/api/shorten`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-    original_url: originalUrl,
-    custom_alias: customAlias || null,
-    expiry_days: expiryDays
-        ? parseInt(expiryDays)
-        : null,
-}),
+      original_url: originalUrl,
+      custom_alias: customAlias || null,
+      expiry_days: expiryDays ? parseInt(expiryDays) : null,
+    }),
   });
 
   return response.json();
